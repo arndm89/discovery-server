8946246892afae4e0523c76b2e536e9bd5b9bb6c
Arindam Chowdhury <arndm89@gmail.com>
2020-06-24 15:40:30 +0530

    initial commit

---
+++ src/main/java/com/cts/eureka/DiscoveryServerApplication.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.cts.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ctsjavafsd24
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
